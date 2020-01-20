7fb04081e5cb06043489f88627287295582bdfc9
Niklas Salmoukas <niklas@salmoukas.com>
2020-01-20 17:10:14 +0100

    first draft

---
+++ main.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const { pack } = require('./pack');
+
+if (process.argv.length != 4) {
+    console.log(`Usage: ${process.argv[0]} ${process.argv[1]} input.py output.py`);
+    process.exit(1);
+}
+
+try {
+    const packed = pack(process.argv[2]);
+    fs.writeFileSync(process.argv[3], packed, { encoding: 'utf8' });
+    process.exit(0);
+}
+catch (error) {
+    console.error(`An error occured: ${error.message || error}`);
+    process.exit(2);
+}
---
+++ pack.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+function escape(script) {
+    return script.split('\\').join('\\\\').split('\'').join('\\\'');
+}
+
+module.exports.pack = function pack(scriptPath) {
+    const scriptName = path.basename(scriptPath);
+    const dirPath = path.dirname(scriptPath);
+    const script = fs.readFileSync(scriptPath, { encoding: 'utf8' });
+    let lines = script.split(/\r?\n/);
+    lines = lines.map(line => {
+        const m = line.match(/^from\s+\.\s+import\s+(\w+)\s*$/);
+        if (!m) {
+            return line;
+        }
+        return (
+            `import sys, importlib\n`
+            + `${m[1]} = importlib.util.module_from_spec(importlib.util.spec_from_loader(__name__${scriptName != '__init__.py' ? '.rsplit(\'.\', 1)[0]' : ''} + '.${m[1]}', loader=None))\n`
+            + `exec('''${escape(pack(path.join(dirPath, m[1] + '.py')))}''', ${m[1]}.__dict__)\n`
+            + `sys.modules[__name__${scriptName != '__init__.py' ? '.rsplit(\'.\', 1)[0]' : ''} + '.${m[1]}'] = ${m[1]}`
+        );
+    });
+    return lines.join('\n');
+}
