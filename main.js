12ac7b00fbf9940bcdade7413303e32a163a25a9
Niklas Salmoukas <niklas@salmoukas.com>
2020-02-03 13:22:13 +0100

    improve packaging algorithm

--- main.js
+++ main.js
@@ -3,14 +3,21 @@
 const fs = require('fs');
 const { pack } = require('./pack');
 
-if (process.argv.length != 4) {
-    console.log(`Usage: ${process.argv[0]} ${process.argv[1]} input.py output.py`);
-    process.exit(1);
-}
-
 try {
-    const packed = pack(process.argv[2]);
-    fs.writeFileSync(process.argv[3], packed, { encoding: 'utf8' });
+    // check arguments
+    if (process.argv.length < 4) {
+        console.log(`Usage: ${process.argv[0]} ${process.argv[1]} <output-path> <module-path> [<library-path> ...]`);
+        process.exit(1);
+    }
+    
+    // extract arguments
+    const outputPath = process.argv[2];
+    const modulePath = process.argv[3];
+    const libraryPaths = process.argv.slice(4);
+
+    // pack
+    const packed = pack(modulePath, libraryPaths);
+    fs.writeFileSync(outputPath, packed, { encoding: 'utf8' });
     process.exit(0);
 }
 catch (error) {
--- pack.js
+++ pack.js
@@ -5,22 +5,97 @@ function escape(script) {
     return script.split('\\').join('\\\\').split('\'').join('\\\'');
 }
 
-module.exports.pack = function pack(scriptPath) {
-    const scriptName = path.basename(scriptPath);
-    const dirPath = path.dirname(scriptPath);
-    const script = fs.readFileSync(scriptPath, { encoding: 'utf8' });
-    let lines = script.split(/\r?\n/);
-    lines = lines.map(line => {
-        const m = line.match(/^from\s+\.\s+import\s+(\w+)\s*$/);
-        if (!m) {
-            return line;
+function readPackList(refPackListPath) {
+    return fs
+        .readFileSync(refPackListPath, { encoding: 'utf8' })
+        .split(/\r?\n/)
+        .map(line => line.trim())
+        .filter(line => line);
+}
+
+function packEntries(modulePath, moduleName, packList, libraryPaths) {
+
+    const result = new Map();
+
+    for (const itemName of packList) {
+        // handle library reference
+        if (!itemName.startsWith('.')) {
+            // search in library paths
+            let found = false;
+            for (const libraryPath of libraryPaths) {
+                // construct paths of reference module
+                const refModulePath = path.join(libraryPath, ...itemName.split('.'));
+                const refPackListPath = path.join(refModulePath, 'pack.list');
+                // if pack list exists, pack module entries
+                if (fs.existsSync(refPackListPath)) {
+                    const refPackList = readPackList(refPackListPath);
+                    packEntries(refModulePath, itemName, refPackList, libraryPaths).forEach((v, k) => {
+                        if (!result.has(k)) {
+                            result.set(k, v);
+                        } else {
+                            if (result.get(k) !== v) {
+                                throw new Error(`two different scripts for ${k} detected`);
+                            }
+                        }
+                    });
+                    found = true;
+                    break;
+                }
+            }
+            // if we reach here, we could not find the reference module
+            if (!found) {
+                throw new Error(`could not find ${itemName} in library paths (pack.list available?)`);
+            }
         }
-        return (
-            `import sys, importlib.util\n`
-            + `${m[1]} = importlib.util.module_from_spec(importlib.util.spec_from_loader(__name__${scriptName != '__init__.py' ? '.rsplit(\'.\', 1)[0]' : ''} + '.${m[1]}', loader=None))\n`
-            + `exec('''${escape(pack(path.join(dirPath, m[1] + '.py')))}''', ${m[1]}.__dict__)\n`
-            + `sys.modules[__name__${scriptName != '__init__.py' ? '.rsplit(\'.\', 1)[0]' : ''} + '.${m[1]}'] = ${m[1]}`
-        );
-    });
-    return lines.join('\n');
+        // handle local file reference
+        else {
+            // lookup item path
+            const itemPathBase = path.join(modulePath, ...itemName.substr(1).split('.'));
+            const itemPath = [itemPathBase + '.py', path.join(itemPathBase, '__init__.py')]
+                .filter(itemPath => fs.existsSync(itemPath))
+                .pop();
+
+            if (!itemPath) {
+                throw new Error(`could not find ${itemName} in module`);
+            }
+
+            // build qualified name
+            const itemNameQualified =
+                (moduleName.endsWith('.') ? moduleName.substr(0, moduleName.length - 1) : moduleName)
+                + (itemName.endsWith('.') ? itemName.substr(0, itemName.length - 1) : itemName);
+            const itemNameCode = (itemNameQualified.startsWith('.') ? '__name__ + ' : '') + JSON.stringify(itemNameQualified);
+
+            // pack
+            result.set(
+                itemNameQualified,
+                `sys.modules[${itemNameCode}] = importlib.util.module_from_spec(importlib.util.spec_from_loader(${itemNameCode}, loader=None))\n`
+                + `exec('''${escape(fs.readFileSync(itemPath, { encoding: 'utf8' }))}''', sys.modules[${itemNameCode}].__dict__)\n`
+            )
+        }
+    }
+
+    return result;
+}
+
+module.exports.pack = function pack(modulePath, libraryPaths) {
+
+    // read module file and pack list
+    const moduleInitFile = fs.readFileSync(path.join(modulePath, '__init__.py'), { encoding: 'utf8' }).split(/\r?\n/);
+    const packList = readPackList(path.join(modulePath, 'pack.list'));
+
+    if (packList.pop() != '.') {
+        throw new Error('last entry of initial pack list should be "."');
+    }
+
+    const packedEntries = packEntries(modulePath, '.', packList, libraryPaths)
+
+    // find first import line
+    let insertIdx = moduleInitFile.findIndex(line => line.match(/(^|\s+)import(\s+|$)/));
+    if (insertIdx === -1) {
+        insertIdx = moduleInitFile.length;
+    }
+
+    // insert packed entries and assemble result
+    moduleInitFile.splice(insertIdx, 0, `\n\nimport sys, importlib.util\n\n` + [...packedEntries.values()].join('\n'));
+    return moduleInitFile.join('\n');
 }
